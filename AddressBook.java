24dd1918d96bec3b7151eac9d4e82a77e89dbcda
k12e <kalyanirathod2095@gmail.com>
2022-04-05 13:16:09 +0530

    first commit

---
+++ AddressBook.java
@@ -0,0 +1,18 @@
+package day4Problems;
+
+public class AddressBook {
+	
+	public static void  contacts()
+	{
+		String firstName;
+		String lastName;
+		String address;
+		String city;
+		String state;
+		int phoneno;
+		String email;
+		
+	}
+
+	
+}
---
+++ AddressBookMain.java
@@ -0,0 +1,10 @@
+package day4Problems;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("**WELCOME**");                                                        
+	}
+
+}
